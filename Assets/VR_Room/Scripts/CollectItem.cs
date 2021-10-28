fb2e3797d4a8f3641fb6304ea7e46f8a394148a8
AmosDong <donghaonan1997@outlook.com>
2021-10-28 12:02:32 -0600

    Part of Functions Implemented

---
+++ Assets/VR_Room/Scripts/CollectItem.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CollectItem : MonoBehaviour
+{
+    public bool HaveToReleaseItem = true;
+    public Image image_answer;
+  
+    void Start()
+    {
+        image_answer.gameObject.SetActive(false);
+    }
+
+    void OnCollisionEnter(Collision col)
+    {
+        if (col.gameObject.name == "SphereForTest")
+        {
+            if (HaveToReleaseItem && col.gameObject.transform.Find("Obj_Script").GetComponent<ModifyStatus>().selected)
+                return;
+            image_answer.gameObject.SetActive(true);
+            col.gameObject.SetActive(false);           
+        };
+    }
+
+
+}
---
+++ Assets/VR_Room/Scripts/ModifyStatus.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ModifyStatus : MonoBehaviour
+{
+    public bool selected = false;
+
+    public void ChangeTo_Selected()
+    {
+        selected = true;
+    }
+
+    public void ChangeTo_DeSelected()
+    {
+        selected = false;
+    }
+}
